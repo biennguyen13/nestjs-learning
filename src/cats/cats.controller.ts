b306f1ca999a403f26951a38c7f46a32edf77278
BiênIsMyName <40268416+biennguyen13@users.noreply.github.com>
2023-03-16 17:48:44 +0700

    test

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats';
+  }
+  @Get('breed')
+  getBreed(): string {
+    return 'This action returns breed';
+  }
+}
